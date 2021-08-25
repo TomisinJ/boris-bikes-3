6db951715120198587213f8563118f3ae0812697
Keldra <KeldraSJ@gmail.com>
2021-08-25 16:45:04 +0100

    first commit for release bike method

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,3 +1,4 @@
 class Docking_Station
-
+  def release_bike
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,4 @@
 require "docking_station.rb"
 describe Docking_Station do
-
+  it { is_expected.to respond_to :release_bike } 
 end
