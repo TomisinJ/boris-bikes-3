d959b2a1f6218e99b99ed0bcd00699c38f214d60
Ifrah Hussein <Ifrahhssn@outlook.com>
2021-08-26 17:23:48 +0100

    Created unit tests and used instance variables

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,11 @@
 require_relative 'Bike'
 
 class Docking_Station
-  def release_bike
-  Bike.new 
-  end
+  attr_reader :bike
+ def release_bike
+   Bike.new
+ end
+ def dock(bike)
+   @bike = bike
+ end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,5 +4,17 @@ describe Docking_Station do
   it "will release a working bike" do
     bike = subject.release_bike
     expect(bike).to be_working
+  end 
+  
+  it "docks bike" do 
+    bike = Bike.new 
+    expect(subject.dock(bike)).to eq bike 
   end
+
+  it "return docked bike" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end 
+  
 end
